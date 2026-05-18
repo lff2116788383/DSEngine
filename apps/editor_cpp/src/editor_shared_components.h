212abea89884ab3b64203d1e290e271a724d3030
qq2116788383 <2116788383@qq.com>
2026-05-18 13:44:49 +0800

    refactor(editor): move EditorNameComponent/SiblingIndexComponent into namespace dse::editor (with using aliases)

--- apps/editor_cpp/src/editor_shared_components.h
+++ apps/editor_cpp/src/editor_shared_components.h
@@ -2,6 +2,8 @@
 
 #include <string>
 
+namespace dse::editor {
+
 struct EditorNameComponent {
     std::string name;
 };
@@ -9,3 +11,9 @@ struct EditorNameComponent {
 struct SiblingIndexComponent {
     int index = 0;
 };
+
+} // namespace dse::editor
+
+// Keep short aliases for backward compatibility in files that don't use the namespace
+using dse::editor::EditorNameComponent;
+using dse::editor::SiblingIndexComponent;
